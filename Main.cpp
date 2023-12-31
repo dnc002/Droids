1bfbcbf6ab1d306796ed88a97dd00a03449e4777
dnc002 <44558359+dnc002@users.noreply.github.com>
2023-12-30 23:39:13 -0800

    Initial Commit
    Using the "Hello, SFML" project. Also uses a console window for debug and informational output.

---
+++ Main.cpp
@@ -0,0 +1,34 @@
+#define SFML_STATIC
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	std::cout << "Hello, SFML" << std::endl;
+
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	std::cout << "Window Closed" << std::endl;
+
+	return 0;
+}
---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# Droids
+
+A project to learn the basics of SFML. Using SFML 2.6.1 64 bit and MSVC 17.
